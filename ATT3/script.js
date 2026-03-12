c207460cef11b212bb9429ea0ce094d326f4d8a8
Leonardo-Campos-2007 <leonardogomescampos2007@gmail.com>
2026-03-12 12:11:23 -0300

    add

---
+++ ATT3/script.js
@@ -0,0 +1,29 @@
+let meuElemento = document.getElementById("paragrafo");
+console.log(meuElemento);
+console.log(meuElemento.textContent);
+
+let paragrafo1 = document.getElementsByClassName("paragrafo");
+console.log(paragrafo1);
+for (let i=0; i < paragrafo1.length; i++) {
+    console.log(paragrafo1[i].textContent);
+}
+
+let paragrafo2 = document.getElementsByTagName("p");
+console.log(paragrafo2);
+
+let destino = document.getElementById("elemento");
+let p = document.createElement("p");
+p.textContent = "Parágrafo criado via JS";
+destino.append(p);
+
+let lista = document.getElementById("lista");
+let ul = document.createElement("ul");
+let itens = ["Arroz", "Feijão", "Picanha", "Mineiro", "Coca-cola"];
+for (let i = 0; i < itens.length; i++){
+    let li = document.createElement("li");
+    li.textContent = itens[i];
+    ul.append(li);
+
+}
+
+lista.append(ul);
