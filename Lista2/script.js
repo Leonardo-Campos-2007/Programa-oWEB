6f09be69841126a213a81402d5957d3f5422ca80
Leonardo-Campos-2007 <leonardogomescampos2007@gmail.com>
2026-03-19 16:34:17 -0300

    add

--- Lista2/script.js
+++ Lista2/script.js
@@ -24,14 +24,14 @@ let caixa = document.getElementById("caixa");
 let paragrafo = document.getElementById("paragrafo")
 let btn = document.getElementById("btn")
 
-btn.onclick = function(){
+btn.onclick = function () {
     paragrafo.innerHTML += "<br>" + caixa.value
 }
 
 
 let contador2 = document.getElementById("contador2");
 
-caixa.addEventListener("input", function(){
+caixa.addEventListener("input", function () {
     let semEspacos = caixa.value.replace(/ /g, "");
     contador2.textContent = semEspacos.length;
 });
@@ -41,10 +41,18 @@ let ordenada = document.getElementById("ordenada");
 let naoordenada = document.getElementById("naoordenada");
 let tipolista = document.getElementById("tipolista");
 
-ordenada.onclick = function(){
+ordenada.onclick = function () {
     tipolista.innerHTML = "<ol><li>Primeiro</li><li>Segundo</li></ol>";
 }
 
-naoordenada.onclick = function(){
-     tipolista.innerHTML = "<ul><li>Primeiro</li><li>Segundo</li></ul>";
+naoordenada.onclick = function () {
+    tipolista.innerHTML = "<ul><li>Primeiro</li><li>Segundo</li></ul>";
+}
+
+let reset = document.getElementById("reset");
+
+reset.onclick = function () {
+
+    location.reload();
+
 }
