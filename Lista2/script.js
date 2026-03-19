90b0a28ab3db87939eb90b4ccdac44014014948b
Leonardo-Campos-2007 <leonardogomescampos2007@gmail.com>
2026-03-19 16:23:36 -0300

    add

--- Lista2/script.js
+++ Lista2/script.js
@@ -1,6 +1,6 @@
 let adicionar = document.getElementById("adicionar");
 let retirar = document.getElementById("retirar");
-let contador = document.getElementById9("contador");
+let contador = document.getElementById("contador");
 let saida = 0;
 
 adicionar.onclick = function () {
@@ -13,4 +13,38 @@ adicionar.onclick = function () {
 retirar.onclick = function () {
     saida--;
     contador.innerHTML = saida;
+    if (saida < 0) {
+        alert("Valor negativo");
+        saida = 0;
+    }
+}
+
+
+let caixa = document.getElementById("caixa");
+let paragrafo = document.getElementById("paragrafo")
+let btn = document.getElementById("btn")
+
+btn.onclick = function(){
+    paragrafo.innerHTML += "<br>" + caixa.value
+}
+
+
+let contador2 = document.getElementById("contador2");
+
+caixa.addEventListener("input", function(){
+    let semEspacos = caixa.value.replace(/ /g, "");
+    contador2.textContent = semEspacos.length;
+});
+
+
+let ordenada = document.getElementById("ordenada");
+let naoordenada = document.getElementById("naoordenada");
+let tipolista = document.getElementById("tipolista");
+
+ordenada.onclick = function(){
+    tipolista.innerHTML = "<ol><li>Primeiro</li><li>Segundo</li></ol>";
+}
+
+naoordenada.onclick = function(){
+     tipolista.innerHTML = "<ul><li>Primeiro</li><li>Segundo</li></ul>";
 }
