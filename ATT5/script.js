24c64b5a424b992c38d2ec500118371ab52aeb14
Leonardo-Campos-2007 <leonardogomescampos2007@gmail.com>
2026-03-26 11:47:12 -0300

    add

---
+++ ATT5/script.js
@@ -0,0 +1,15 @@
+function somar (a, b){
+    return a + b;
+}
+
+function saldacao(nome){
+    console.log(`Olá, ${nome}!`)
+}
+
+var nome = saldacao(leo)
+
+console.log(nome)
+
+var resultado = somar(5, 10)
+
+console.log(resultado)
