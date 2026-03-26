693e6be70737ece15680e9007798b0be75a3e702
Leonardo-Campos-2007 <leonardogomescampos2007@gmail.com>
2026-03-26 12:55:37 -0300

    add

--- ATT5/script.js
+++ ATT5/script.js
@@ -1,15 +1,84 @@
-function somar (a, b){
-    return a + b;
+const precoGasolina = 6.69
+const precoEtanol = 4.30
+const precoDiesel = 6.03
+
+function atualizarValor() {
+    let tipo = document.getElementById("combustivel").value
+    console.log(tipo)
+
+    let precoPorLitro
+    switch (tipo) {
+
+        case 'gasolina':
+            precoPorLitro = precoGasolina;
+            break;
+
+        case 'etanol':
+            precoPorLitro = precoEtanol
+            break;
+
+        case 'diesel':
+            precoPorLitro = precoDiesel
+            break;
+
+        default:
+            console.log("ESCOLHA UMA OPÇÃO")
+            return;
+
+    }
+
+    console.log(precoPorLitro);
+    let litros = parseFloat(document.getElementById("litros").value);
+    calcularValorAbastecimento(precoPorLitro, litros)
 }
 
-function saldacao(nome){
-    console.log(`Olá, ${nome}!`)
+let tipoCobustivel = document.getElementById("combustivel")
+tipoCobustivel.addEventListener("change", atualizarValor)
+
+function calcularValorAbastecimento(precoCombustivel, litros){
+    //let valorTotal = precoCombustivel * litros;
+   //docu.getElementById("resultado").textContent = valorTotal
+
+   if(litros <= 0 || isNaN(litros) ){
+    document.getElementById("resultado").textContent = "Insira um valor valido"
+    return;
+   }else{
+    let valorTotal = precoCombustivel * litros;
+    document.getElementById("resultado").textContent = `R$: ${valorTotal.toLocaleString('pt-BR', { minimumFractionDigits: 2, maximumFractionDigits: 2 })}`
+   }
+
+ 
+   
+
+   
 }
+let litros = document.getElementById("litros")
+   litros.addEventListener("input", atualizarValor)
+   
+   litros.addEventListener("keydown", function(event){
+    if(event.key == "Enter"){
+        event.preventDefault();
+        atualizarValor();
+    }
+   })
 
-var nome = saldacao(leo)
 
-console.log(nome)
 
-var resultado = somar(5, 10)
 
-console.log(resultado)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
