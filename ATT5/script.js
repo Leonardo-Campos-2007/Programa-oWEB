5bf72729503390d27357056acff693a6668361db
Leonardo-Campos-2007 <leonardogomescampos2007@gmail.com>
2026-03-30 21:24:35 -0300

    add

--- ATT5/script.js
+++ ATT5/script.js
@@ -2,65 +2,79 @@ const precoGasolina = 6.69
 const precoEtanol = 4.30
 const precoDiesel = 6.03
 
-function atualizarValor() {
-    let tipo = document.getElementById("combustivel").value
-    console.log(tipo)
+const precoReal = 1.00
+const precoDolar = 5.30
+const precoEuro = 6.20
 
-    let precoPorLitro
-    switch (tipo) {
+const atualizarValor = () => {
+    let tipo = document.getElementById("combustivel").value;
+    let tipomoeda = document.getElementById("dinheiro").value;
 
-        case 'gasolina':
-            precoPorLitro = precoGasolina;
-            break;
+    let precoPorLitro;
 
-        case 'etanol':
-            precoPorLitro = precoEtanol
-            break;
-
-        case 'diesel':
-            precoPorLitro = precoDiesel
-            break;
-
-        default:
-            console.log("ESCOLHA UMA OPÇÃO")
-            return;
-
+    if (tipo == 'gasolina') precoPorLitro = precoGasolina;
+    else if (tipo == 'etanol') precoPorLitro = precoEtanol;
+    else if (tipo == 'diesel') precoPorLitro = precoDiesel;
+    else {
+        console.log("Escolha um combustível");
+        return;
     }
 
-    console.log(precoPorLitro);
+
+    if (tipomoeda == 'dolar') precoPorLitro = precoPorLitro / precoDolar;
+    else if (tipomoeda == 'euro') precoPorLitro = precoPorLitro / precoEuro;
+
+
     let litros = parseFloat(document.getElementById("litros").value);
-    calcularValorAbastecimento(precoPorLitro, litros)
-}
+    calcularValorAbastecimento(precoPorLitro, litros, tipomoeda);
+};
 
 let tipoCobustivel = document.getElementById("combustivel")
 tipoCobustivel.addEventListener("change", atualizarValor)
 
-function calcularValorAbastecimento(precoCombustivel, litros){
+const calcularValorAbastecimento = (precoCombustivel, litros, tipodamoeda) => {
     //let valorTotal = precoCombustivel * litros;
-   //docu.getElementById("resultado").textContent = valorTotal
+    //docu.getElementById("resultado").textContent = valorTotal
 
-   if(litros <= 0 || isNaN(litros) ){
-    document.getElementById("resultado").textContent = "Insira um valor valido"
-    return;
-   }else{
-    let valorTotal = precoCombustivel * litros;
-    document.getElementById("resultado").textContent = `R$: ${valorTotal.toLocaleString('pt-BR', { minimumFractionDigits: 2, maximumFractionDigits: 2 })}`
-   }
-
- 
-   
-
-   
-}
-let litros = document.getElementById("litros")
-   litros.addEventListener("input", atualizarValor)
-   
-   litros.addEventListener("keydown", function(event){
-    if(event.key == "Enter"){
-        event.preventDefault();
-        atualizarValor();
+    if (litros <= 0 || isNaN(litros)) {
+        document.getElementById("resultado").textContent = "Insira um valor valido"
+        return;
+    } else {
+        if (tipodamoeda == 'real') {
+            let valorTotal = precoCombustivel * litros;
+            document.getElementById("resultado").textContent = `R$: ${valorTotal.toLocaleString('pt-BR', { minimumFractionDigits: 2, maximumFractionDigits: 2 })}`
+        }
+        else if (tipodamoeda == 'dolar') {
+            let valorTotal = precoCombustivel * litros;
+            document.getElementById("resultado").textContent = `US$: ${valorTotal.toLocaleString('en-US', { minimumFractionDigits: 2, maximumFractionDigits: 2 })}`
+        }
+        else if (tipodamoeda == 'euro') {
+            let valorTotal = precoCombustivel * litros;
+            document.getElementById("resultado").textContent = `€: ${valorTotal.toLocaleString('de-DE', { minimumFractionDigits: 2, maximumFractionDigits: 2 })}`
+        }
     }
-   })
+
+
+}
+
+
+
+
+
+let litros = document.getElementById("litros")
+litros.addEventListener("input", atualizarValor)
+
+const anonima = () => {
+
+    litros.addEventListener("keydown", function (event) {
+        if (event.key == "Enter") {
+            event.preventDefault();
+            atualizarValor();
+        }
+    })
+
+}
+
 
 
 
