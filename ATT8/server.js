edc6b0ef98a26aeb01f40219d1c41d1dd00b9c76
Leonardo-Campos-2007 <leonardogomescampos2007@gmail.com>
2026-05-07 12:22:13 -0300

    Add HTML and JavaScript files for main and about pages, implement routing and static file serving

--- ATT8/server.js
+++ ATT8/server.js
@@ -2,8 +2,24 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+app.use(express.static('public'));
+
+
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+ // res.send('Hello World!')
+ res.sendFile(__dirname + '/public/index.html');
+})
+
+app.get('/sobre', (req, res) => {
+  res.sendFile(__dirname + '/public/sobre.html');
+})
+
+app.get('/sobre/:id', (req, res) => {
+  res.send(`Meu nome é ${req.params.id}`)
+})
+
+app.get('/sobre/paocomacai', (req, res) => {
+  res.send('Pão com Açaí')
 })
 
 app.listen(port, () => {
