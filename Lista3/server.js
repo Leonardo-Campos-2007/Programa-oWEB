b46f57df3370ade3ed3054470194c75616ba8c12
Leonardo-Campos-2007 <leonardogomescampos2007@gmail.com>
2026-05-11 13:01:16 -0300

    add

--- Lista3/server.js
+++ Lista3/server.js
@@ -7,6 +7,21 @@ app.use(express.static('public'));
 
 app.use(express.static('public'));
 
+const feedbacks = []
+
+app.listen(port, () => {
+    console.log(`Servidor rodando em http://localhost:${port}`)
+})
+
 app.get('/', (req, res)=>{
     res.sendFile(__dirname + '/public/index.html')
+})
+app.get('/feedbacks', (req, res)=>{
+    res.sendFile(__dirname + '/public/feedbacks.html')
+})
+
+
+
+app.get('/feedbacks', (req, res) => {
+    res.json(feedbacks) // retorna os dados em JSON
 })
